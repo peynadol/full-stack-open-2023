e8910f3dcd000f7e11d4cf773ab01a4b479fd1e8
peynadol <101970481+peynadol@users.noreply.github.com>
2023-05-23 21:48:11 +0100

    part1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,3 +1,27 @@
+const Header = (props) => {
+  return <h1>{props.course} </h1>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      Title: "{props.part}" Exercises: {props.exercise}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part} exercise={props.exercise} />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return <p>Total number of exercises: {props.total}</p>;
+};
+
 const App = () => {
   const course = 'Half Stack application development';
   const part1 = 'Fundamentals of React';
@@ -9,17 +33,12 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part={part1} exercise={exercises1} />
+      <Content part={part2} exercise={exercises2} />
+      <Content part={part3} exercise={exercises3} />
+
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   );
 };
